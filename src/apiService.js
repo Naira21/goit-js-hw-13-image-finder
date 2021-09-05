01c34600fa90f3c52e32783a90aa99477e48fa25
Naira Kazeian <nkazeian@gmail.com>
2021-09-05 23:43:00 +0300

    added functions, class

--- src/apiService.js
+++ src/apiService.js
@@ -1,11 +1,28 @@
-export default function onSearch(e) {
-   const searchQuery = e.currentTarget.elements.query.value;
+export default class NewApiSearch {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    fetchImages() {
+       const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=23235889-ad2e782c3a775466fc04cd861`;
 
-    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=номер_страницы&per_page=12&key=23235889-ad2e782c3a775466fc04cd861`;
+        return fetch(url)
+            .then(response => response.json())
+            .then(({images})=> {
+                this.page += 1;
+                return images;
+            });
+    }
 
-    fetch(url)
-    .then(array => array.json())
-    .catch(er => onError);
- 
+    get query() {
+        return this.searchQuery;
+    }
+    
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+    resetPage() {
+        this.page = 1;
+    }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
-import './sass/main.scss';
-import './apiService';
-import cardTemplate from './templates/card.hbs';
+import './sass/main.scss'
+import NewApiSearch from './apiService'
+// import cardTemplate from './templates/card.hbs';
 import formTemplate from './templates/form.hbs';
 import galleryTemplate from './templates/gallery.hbs';
 
 const input = document.querySelector('#search-form');
+const loadButton = document.querySelector('[data-action="load-more"]');
+const bodyRef = document.querySelector('body');
+
 input.addEventListener('submit', onSearch);
+loadButton.addEventListener('click', clickLoadBtn);
+bodyRef.addEventListener('DOMContentLoaded', renderMarkup(formTemplate, images));
+
+const newApiSearch = new NewApiSearch();
+console.log(newApiSearch);
+
+function onSearch(e) {
+    e.preventDefault();
+    newApiSearch.query = e.currentTarget.elements.query.value;
+    newApiSearch.resetPage();
+    clearGalleryResult();
+    newApiSearch.fetchImages().then(renderCardMarkup);
+
+    if (newApiSearch.query = '') {
+        return alert('Error! Incorrect word. Please, check the query ')
+    }
+}
+
+function clickLoadBtn() {
+  newApiSearch.fetchImages().then(renderCardMarkup);  
+}
+
+// function renderMarkup(template, images) {
+//     bodyRef.insertAdjacentHTML('beforeend', template(images))
+// }
+
+function renderCardMarkup(images) {
+    bodyRef.insertAdjacentHTML('beforeend', galleryTemplate(images))
+}
+
+function clearGalleryResult() {
+    bodyRef.innerHTML = '';
+}
+
+
+//Страница автоматически плавно проскроливается после рендера изображений
+const element = document.getElementById('.my-element-selector');
+element.scrollIntoView({
+  behavior: 'smooth',
+  block: 'end',
+});
