ec82754bef347da6e26c405626e0a7e32b94b468
Naira Kazeian <nkazeian@gmail.com>
2021-09-07 15:11:28 +0300

    added visibility to "load-more" button

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import galleryTemplate from './templates/gallery.hbs';
 const form = document.querySelector('#search-form');
 const loadButton = document.querySelector('[data-action="load-more"]');
 const galleryList = document.querySelector('.gallery');
-// const searchButton = document.querySelector('#submit');
-// console.log(searchButton);
+
 
 form.addEventListener('submit', onSearch);
-// searchButton.addEventListener('submit', onSearch);
 loadButton.addEventListener('click', clickLoadBtn);
 
+loadButton.style.visibility = 'hidden';
 
 const searchProps = {
   searchQuery: undefined,
@@ -25,10 +24,19 @@ async function onSearch(e) {
   searchProps.searchQuery = e.target.query.value;
   const data = await newApiSearch.fetchImages(searchProps.searchQuery);
 
-  if (data.length === 0) {
-    console.log('Error! Check your request');;
+  if (data.length === 0 ) {
+    return alert('Incorrect word! Check your request');
   } renderCardMarkup(data);
 
+  if (searchProps.searchQuery === '') {
+    clearGalleryResult();
+    return alert('Incorrect word! Check your request');
+  }
+  
+  if (data.length > 11) {
+    loadButton.style.visibility = 'visible';
+  }
+  
   loadButton.scrollIntoView({
     behavior: 'smooth',
     block: 'end',
@@ -38,7 +46,8 @@ async function onSearch(e) {
 async function clickLoadBtn() {
   newApiSearch.increment();
   const data = await newApiSearch.fetchImages(searchProps.searchQuery);
-  renderCardMarkup(data);
+  renderCardMarkup(data); 
+  
 };
 
 
@@ -47,7 +56,8 @@ function renderCardMarkup(data) {
 }
 
 function clearGalleryResult() {
-  galleryList.innerHTML = '';  
+  galleryList.innerHTML = '';
+  loadButton.style.visibility = 'hidden';
 }
 
 
